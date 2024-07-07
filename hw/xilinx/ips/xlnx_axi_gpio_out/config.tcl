3c617f9ab46e8ad6177ec645db304e267f443f37
Zaira Abdel Majid <93708921+Za-i-ra@users.noreply.github.com>
2024-07-07 09:39:09 +0200

    Update config.tcl

--- hw/xilinx/ips/xlnx_axi_gpio_out/config.tcl
+++ hw/xilinx/ips/xlnx_axi_gpio_out/config.tcl
@@ -2,9 +2,9 @@
 # Import IP
 create_ip -name axi_gpio -vendor xilinx.com -library ip -version 2.0 -module_name $::env(IP_NAME)
 # Configure IP
-set_property -dict [list CONFIG.C_GPIO_WIDTH {1} 
-                        CONFIG.C_ALL_INPUTS  {0} 
-                        CONFIG.C_ALL_OUTPUTS {1}
-                        CONFIG.C_IS_DUAL     {0}
+set_property -dict [list CONFIG.C_GPIO_WIDTH {1} \
+                        CONFIG.C_ALL_INPUTS  {0} \
+                        CONFIG.C_ALL_OUTPUTS {1} \
+                        CONFIG.C_IS_DUAL     {0} \
                     ] [get_ips $::env(IP_NAME)]
 
