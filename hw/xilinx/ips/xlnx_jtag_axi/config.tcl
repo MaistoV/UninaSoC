fd077c5baed078f85dd36d7a7bf435a4103c1775
Zaira Abdel Majid <93708921+Za-i-ra@users.noreply.github.com>
2024-07-07 09:56:54 +0200

    Update config.tcl

--- hw/xilinx/ips/xlnx_jtag_axi/config.tcl
+++ hw/xilinx/ips/xlnx_jtag_axi/config.tcl
@@ -4,11 +4,11 @@ create_ip -name jtag_axi -vendor xilinx.com -library ip -version 1.2 -module_nam
 # Configure IP
 # - CONFIG.PROTOCOL {0} is AXI4, {2} is AXI4LITE
 # - CONFIG.M_HAS_BURST {1} ALL BURST TYPES, {0} INCR BURST ONLY
-set_property -dict [list CONFIG.PROTOCOL {0} 
-                        CONFIG.M_AXI_DATA_WIDTH {32} 
-                        CONFIG.M_AXI_ADDR_WIDTH {32} 
-                        CONFIG.M_AXI_ID_WIDTH {1}
-                        CONFIG.RD_TXN_QUEUE_LENGTH {1} 
-                        CONFIG.WR_TXN_QUEUE_LENGTH {1} 
-                        CONFIG.M_HAS_BURST {1} 
+set_property -dict [list CONFIG.PROTOCOL {0} \
+                        CONFIG.M_AXI_DATA_WIDTH {32} \
+                        CONFIG.M_AXI_ADDR_WIDTH {32} \
+                        CONFIG.M_AXI_ID_WIDTH {1} \
+                        CONFIG.RD_TXN_QUEUE_LENGTH {1} \
+                        CONFIG.WR_TXN_QUEUE_LENGTH {1} \
+                        CONFIG.M_HAS_BURST {1} \
     ] [get_ips $::env(IP_NAME)]
