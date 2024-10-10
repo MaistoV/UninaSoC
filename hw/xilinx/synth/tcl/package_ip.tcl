5efbdeca983cd52d87d2fee58242c202d1cfbef0
Gr4npasso <stefanomercogliano@gmail.com>
2024-10-10 14:22:04 +0200

    [Flow] Updating the compiling flow to remove dummy dirs

--- hw/xilinx/synth/tcl/package_ip.tcl
+++ hw/xilinx/synth/tcl/package_ip.tcl
@@ -5,14 +5,16 @@ update_compile_order -fileset sources_1
 set_property top ${top_module} [current_fileset]
 
 # Package the IP and update the catalog
-ipx::package_project -root_dir $::env(XILINX_IPS_ROOT)/$::env(IP_PRJ_NAME)/build/$::env(IP_PRJ_NAME).srcs/sources_1/imports -vendor user.org -library user -taxonomy /UserIP
+ipx::package_project -root_dir $::env(IP_DIR)/build/$::env(IP_PRJ_NAME).srcs/sources_1/imports -vendor user.org -library user -taxonomy /UserIP
 set_property name $::env(IP_PRJ_NAME) [ipx::current_core]
 set_property core_revision 2 [ipx::current_core]
 ipx::update_checksums [ipx::current_core]
 ipx::check_integrity [ipx::current_core]
 ipx::save_core [ipx::current_core]
-set_property  ip_repo_paths  $::env(XILINX_IPS_ROOT)/$::env(IP_PRJ_NAME)/build/$::env(IP_PRJ_NAME).srcs/sources_1/imports [current_project]
+set_property  ip_repo_paths  $::env(IP_DIR)/build/$::env(IP_PRJ_NAME).srcs/sources_1/imports [current_project]
 update_ip_catalog
 
 # Import IP into the project
 create_ip -vlnv user.org:user:$::env(IP_PRJ_NAME):1.0 -module_name $::env(IP_NAME) 
+
+#$::env(XILINX_IPS_ROOT)/
