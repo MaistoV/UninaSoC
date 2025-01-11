184320792ecbb20c56cfa92c43c72b32861d7fae
Manuel Maddaluno <manuelmaddaluno5@gmail.com>
2025-01-11 18:34:04 +0100

    [quickfix] Update main.c

--- sw/SoC/examples/hello_world/src/main.c
+++ sw/SoC/examples/hello_world/src/main.c
@@ -1,12 +1,12 @@
 #include "tinyIO.h"
 #include <stdint.h>
 
-extern const volatile uint32_t _peripheral_UART_start;
+extern const volatile uint32_t _peripheral_VIRTUAL_UART_start;
 
 int main()
 { 
 
-  uint32_t uart_base_address = (uint32_t) &_peripheral_UART_start;
+  uint32_t uart_base_address = (uint32_t) &_peripheral_VIRTUAL_UART_start;
 
   tinyIO_init(uart_base_address);
 
