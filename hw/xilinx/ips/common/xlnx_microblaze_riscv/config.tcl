3cdb2c770211b427d5266d13d8083977983ea787
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2025-02-04 11:57:45 +0100

    Refactor microblaze-v config.tcl (untested)

--- hw/xilinx/ips/common/xlnx_microblaze_riscv/config.tcl
+++ hw/xilinx/ips/common/xlnx_microblaze_riscv/config.tcl
@@ -2,46 +2,55 @@
 create_ip -name microblaze_riscv -vendor xilinx.com -library ip -version 1.0 -module_name $::env(IP_NAME)
 
 # Configure IP
-set_property -dict  [list CONFIG.C_DEBUG_ENABLED              {1} \
-                          CONFIG.C_NUMBER_OF_RD_ADDR_BRK      {1} \
-                          CONFIG.C_NUMBER_OF_WR_ADDR_BRK      {1} \
-                          CONFIG.C_DEBUG_EVENT_COUNTERS       {0} \
-                          CONFIG.C_DEBUG_LATENCY_COUNTERS     {0} \
-                          CONFIG.C_D_AXI                      {1} \
-                          CONFIG.C_D_LMB                      {0} \
-                          CONFIG.C_ENABLE_DISCRETE_PORTS      {0} \
-                          CONFIG.C_FAULT_TOLERANT             {0} \
-                          CONFIG.C_FSL_LINKS                  {0} \
-                          CONFIG.C_ILL_INSTR_EXCEPTION        {2} \
-                          CONFIG.C_I_AXI                      {1} \
-                          CONFIG.C_I_LMB                      {0} \
-                          CONFIG.C_LOCKSTEP_SELECT            {0} \
-                          CONFIG.C_MISALIGNED_EXCEPTIONS      {1} \
-                          CONFIG.C_M_AXI_D_BUS_EXCEPTION      {0} \
-                          CONFIG.C_M_AXI_I_BUS_EXCEPTION      {0} \
-                          CONFIG.C_NUMBER_OF_PC_BRK           {1} \
-                          CONFIG.C_NUMBER_OF_RD_ADDR_BRK      {0} \
-                          CONFIG.C_NUMBER_OF_WR_ADDR_BRK      {0} \
-                          CONFIG.C_OPTIMIZATION               {0} \
-                          CONFIG.C_TRACE                      {0} \
-                          CONFIG.C_USE_ATOMIC                 {1} \
-                          CONFIG.C_USE_BARREL                 {1} \
-                          CONFIG.C_USE_BITMAN_A               {0} \
-                          CONFIG.C_USE_BITMAN_B               {0} \
-                          CONFIG.C_USE_BITMAN_C               {0} \
-                          CONFIG.C_USE_BITMAN_S               {0} \
-                          CONFIG.C_USE_BRANCH_TARGET_CACHE    {0} \
-                          CONFIG.C_USE_COMPRESSION            {1} \
-                          CONFIG.C_USE_COUNTERS               {1} \
-                          CONFIG.C_USE_DCACHE                 {0} \
-                          CONFIG.C_USE_FPU                    {1} \
-                          CONFIG.C_USE_ICACHE                 {0} \
-                          CONFIG.C_USE_MMU                    {1} \
-                          CONFIG.C_USE_MULDIV                 {1} \
-                          CONFIG.G_TEMPLATE_LIST              {0} \
-                          CONFIG.C_INTERCONNECT               {2} \
-                          CONFIG.C_DATA_SIZE                  {32} \
-                          CONFIG.C_MMU_PRIVILEGED_INSTR	      {1} \
-                          CONFIG.C_HARTID                     {1} \
-                          CONFIG.C_M_AXI_DP_EXCLUSIVE_ACCESS  {1} 
-                    ] [get_ips $::env(IP_NAME)]
+set_property -dict [list \
+        CONFIG.C_DEBUG_ENABLED {1} \
+        CONFIG.C_ENABLE_DISCRETE_PORTS {0} \
+        CONFIG.C_D_AXI {1} \
+        CONFIG.C_I_AXI {1} \
+        CONFIG.C_USE_DCACHE {0} \
+        CONFIG.C_USE_ICACHE {0} \
+        CONFIG.C_D_LMB {0} \
+        CONFIG.C_I_LMB {0} \
+        CONFIG.C_OPTIMIZATION {0} \
+        CONFIG.C_USE_BARREL {1} \
+        CONFIG.C_USE_COUNTERS {1} \
+        CONFIG.C_M_AXI_D_BUS_EXCEPTION {0} \
+        CONFIG.C_M_AXI_I_BUS_EXCEPTION {0} \
+        CONFIG.C_ILL_INSTR_EXCEPTION {2} \
+    ] [get_ips $::env(IP_NAME)]
+
+###############################
+# AXI interconnect parameters #
+###############################
+# Data width
+set_property CONFIG.C_DATA_SIZE $::env(AXI_DATA_WIDTH) [get_ips $::env(IP_NAME)]
+# Address width is fixed equal to data width
+# set_property CONFIG.ADDR_WIDTH  $::env(AXI_ADDR_WIDTH)  [get_ips $::env(IP_NAME)]
+# AXI ID width is fixed to 1
+# set_property CONFIG.ID_WIDTH    $::env(AXI_ID_WIDTH)    [get_ips $::env(IP_NAME)]
+
+#####################
+# RISC-V Extensions #
+#####################
+# Set configuration preset (1: Microcontroller, 2: Real-time, 4: RVIMC, 5: RVIMAC, 6: RVIMAFC)
+set_property CONFIG.G_TEMPLATE_LIST         {1} [get_ips $::env(IP_NAME)]
+# C-ext
+set_property CONFIG.C_USE_COMPRESSION       {1} [get_ips $::env(IP_NAME)]
+# A-ext
+set_property CONFIG.C_USE_ATOMIC            {0} [get_ips $::env(IP_NAME)]
+# M-ext (1: STANDARD; 2: OPTIMIZED)
+set_property CONFIG.C_USE_MULDIV            {0} [get_ips $::env(IP_NAME)]
+# F-ext
+set_property CONFIG.C_USE_FPU               {0} [get_ips $::env(IP_NAME)]
+# Bit-manipulation
+set_property CONFIG.C_USE_BITMAN_A          {0} [get_ips $::env(IP_NAME)]
+set_property CONFIG.C_USE_BITMAN_B          {0} [get_ips $::env(IP_NAME)]
+set_property CONFIG.C_USE_BITMAN_C          {0} [get_ips $::env(IP_NAME)]
+set_property CONFIG.C_USE_BITMAN_S          {0} [get_ips $::env(IP_NAME)]
+# PMP entries and granularity
+set_property CONFIG.C_PMP_ENTRIES           {0} [get_ips $::env(IP_NAME)]
+set_property CONFIG.C_PMP_GRANULARITY       {2} [get_ips $::env(IP_NAME)]
+# Enable User mode
+set_property CONFIG.C_USE_MMU               {0} [get_ips $::env(IP_NAME)]
+# Illegal instruction exception (1:BASIC, 2: COMPLETE)
+set_property CONFIG.C_ILL_INSTR_EXCEPTION   {2} [get_ips $::env(IP_NAME)]
