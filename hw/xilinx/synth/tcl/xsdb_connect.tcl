4f6ab5f11e2609c6276e72648d53d14f9a4eb4b8
Gr4npasso <stefanomercogliano@gmail.com>
2025-02-14 17:17:01 +0100

    [Makefile] adding the debugging make rules

---
+++ hw/xilinx/synth/tcl/xsdb_connect.tcl
@@ -0,0 +1,29 @@
+# Author: Stefano Mercogliano <stefano.mercogliano@unina.it>
+# Description:
+#   Open XSDB and connect to RISC-V hart 0
+#   The goal is to use XSDB as a backend for GDB
+
+# Connect to hw_server
+connect -host $::env(XILINX_HW_SERVER_HOST) -port $::env(XILINX_HW_SERVER_PORT)
+
+puts "\[INFO\] Initiate connection"
+connect
+
+after 500
+
+# Filter and set target
+puts "\[INFO\] Selecting target Hart #0"
+targets -set -filter {name  =~ "*Hart #0*"}
+# Reset target core
+puts "\[INFO\] Resetting target"
+rst -processor
+
+puts "\[INFO\] XSDB backend running, open GDB"
+
+proc pause {{message "\[INFO\] Press any key to stop"}} {
+    puts -nonewline $message
+    flush stdout
+    gets stdin
+}
+
+pause;                      # Sample usage 2, use default message
