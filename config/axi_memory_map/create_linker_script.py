62d1c44b1587e8aa45eeab1bd1004ea92e216e69
Manuel <manuelmaddaluno5@gmail.com>
2025-02-19 11:36:01 +0100

    [Config] Unify config_main_bus and  config_peripheral_bus target [Config] Modify create_linker_script to parse more than 1 bus

--- config/axi_memory_map/create_linker_script.py
+++ config/axi_memory_map/create_linker_script.py
@@ -25,50 +25,58 @@ import pandas as pd
 ##############
 
 # CSV configuration file path
-config_file_name = 'config/axi_memory_map/configs/config.csv'
+config_file_names = ['config/axi_memory_map/configs/config_main_bus_embedded.csv'] 
 if len(sys.argv) >= 2:
-	config_file_name = sys.argv[1]
+	config_file_names = sys.argv[1:-1]
 
 # Target linker script file
 ld_file_name = 'sw/SoC/common/UninaSoC.ld'
-if len(sys.argv) >= 3:
-	ld_file_name = sys.argv[2]
+if len(sys.argv) >= 4:
+	ld_file_name = sys.argv[-1]
 
 
 ###############
 # Read config #
 ###############
-# Read CSV file
-config_df = pd.read_csv(config_file_name, sep=",", index_col=0)
+# Read CSV files
+config_dfs = []
+for name in config_file_names:
+	config_dfs.append(pd.read_csv(name, sep=",", index_col=0))
 
-# Read number of masters interfaces
-NUM_MI = int(config_df.loc["NUM_MI"]["Value"])
-# print("[DEBUG] NUM_MI", NUM_MI)
+NUM_MI = []
+RANGE_NAMES = []
+RANGE_BASE_ADDR = []
+RANGE_ADDR_WIDTH = []
+for config_df in config_dfs: 
+	# Read number of masters interfaces
+	NUM_MI.append(int(config_df.loc["NUM_MI"]["Value"]))
+	# print("[DEBUG] NUM_MI", NUM_MI)
 
-# Read slaves' names
-RANGE_NAMES = config_df.loc["RANGE_NAMES"]["Value"].split()
-# print("[DEBUG] RANGE_NAMES", RANGE_NAMES)
+	# Read slaves' names
+	RANGE_NAMES.append(config_df.loc["RANGE_NAMES"]["Value"].split())
+	# print("[DEBUG] RANGE_NAMES", RANGE_NAMES)
 
-# Read address Ranges
-RANGE_BASE_ADDR = config_df.loc["RANGE_BASE_ADDR"]["Value"].split()
-# print("[DEBUG] RANGE_BASE_ADDR", RANGE_BASE_ADDR)
+	# Read address Ranges
+	RANGE_BASE_ADDR.append(config_df.loc["RANGE_BASE_ADDR"]["Value"].split())
+	# print("[DEBUG] RANGE_BASE_ADDR", RANGE_BASE_ADDR)
 
-# Read address widths
-RANGE_ADDR_WIDTH = config_df.loc["RANGE_ADDR_WIDTH"]["Value"].split()
-# Turns the values into Integers
-for i in range(len(RANGE_ADDR_WIDTH)):
-	RANGE_ADDR_WIDTH[i] = int(RANGE_ADDR_WIDTH[i])
+	# Read address widths
+	RANGE_ADDR_WIDTH.append(config_df.loc["RANGE_ADDR_WIDTH"]["Value"].split())
+	# Turns the values into Integers
+	for i in range(len(RANGE_ADDR_WIDTH[-1])):
+		RANGE_ADDR_WIDTH[-1][i] = int(RANGE_ADDR_WIDTH[-1][i])
 
 # Currently the first memory device is selected as the boot memory device
 BOOT_MEMORY_BLOCK = 0x0
 
 
-################
-# Sanity check #
-################
-assert (NUM_MI == len(RANGE_NAMES)) & (NUM_MI == len(RANGE_BASE_ADDR) ) & (NUM_MI  == len(RANGE_ADDR_WIDTH)), \
-	"Mismatch in lenght of configurations: NUM_MI(" + str(NUM_MI) + "), RANGE_NAMES (" + str(len(RANGE_NAMES)) + \
-	"), RANGE_BASE_ADDR(" + str(len(RANGE_BASE_ADDR)) + ") RANGE_ADDR_WIDTH(" + str(len(RANGE_ADDR_WIDTH)) + ")"
+# ################
+# # Sanity check #
+# ################
+for i in range(len(NUM_MI)): 
+	assert (NUM_MI[i] == len(RANGE_NAMES[i])) & (NUM_MI[i] == len(RANGE_BASE_ADDR[i]) ) & (NUM_MI[i]  == len(RANGE_ADDR_WIDTH[i])), \
+		"Mismatch in lenght of configurations: NUM_MI(" + str(NUM_MI[i]) + "), RANGE_NAMES (" + str(len(RANGE_NAMES[i])) + \
+		"), RANGE_BASE_ADDR(" + str(len(RANGE_BASE_ADDR[i])) + ") RANGE_ADDR_WIDTH(" + str(len(RANGE_ADDR_WIDTH[i])) + ")"
 
 ##########################
 # Generate memory blocks #
@@ -81,18 +89,24 @@ device_dict = {
 }
 
 counter = 0
-for device in RANGE_NAMES:
-	match device:
-		# memory blocks
-		case "BRAM" | "DDR" | "HBM":
-			device_dict['memory'].append({'device': device, 'base': int(RANGE_BASE_ADDR[counter], 16), 'range': 2 << RANGE_ADDR_WIDTH[counter]})
+for i in range(len(RANGE_NAMES)):
+	for device in RANGE_NAMES[i]:
+		match device:
+			# memory blocks
+			case "BRAM" | "DDR" | "HBM":
+				device_dict['memory'].append({'device': device, 'base': int(RANGE_BASE_ADDR[i][counter], 16), 'range': 2 << RANGE_ADDR_WIDTH[i][counter]})
 
-		# peripherals
-		case _:
-			device_dict['peripheral'].append({'device': device, 'base': int(RANGE_BASE_ADDR[counter], 16), 'range': 2 << RANGE_ADDR_WIDTH[counter]})
+			# peripherals
+			case _:
+				if device[1:] != "BUS":  
+					print(RANGE_BASE_ADDR, RANGE_ADDR_WIDTH) 
+					device_dict['peripheral'].append({'device': device, 'base': int(RANGE_BASE_ADDR[i][counter], 16), 'range': 2 << RANGE_ADDR_WIDTH[i][counter]})
 
-	# Increment counter
-	counter += 1
+		# Increment counter
+		counter += 1
+		if counter == len(RANGE_NAMES[i]):
+			counter = 0
+			
 
 ###############################
 # Generate Linker Script File #
