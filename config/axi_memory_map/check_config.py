473247c0cb4a56ab2db101340d1116c9d24094de
Manuel <manuelmaddaluno5@gmail.com>
2025-03-04 18:47:49 +0100

    [refactor] split inter and intra checks in the main [doc] add detailed description on the checks performed by check_config.py in its file description and add note on the CSV assumption

--- config/axi_memory_map/check_config.py
+++ config/axi_memory_map/check_config.py
@@ -2,6 +2,21 @@
 # Author: Manuel Maddaluno        <manuel.maddaluno@unina.it>
 # Description:
 #   Check the validity of the CSV configurations
+#   The checks are split in two part: 1) intra configuration checks and 2) inter configuration checks
+#   
+#       1) intra configuration checks:
+#           a) check the validity of the given axi protocol (AXI4, AXI4LITE)
+#           b) check the correspondence of NUM_MI with RANGE_NAMES, BASE_ADDR, and RANGE_ADDR_WIDTH (e.g. NUM_MI=2 -> len(RANGE_NAMES)=2 etc.)
+#           c) check the minimum width of each address range (12 if AXI4, 1 if AXI4LITE)
+#           d) check the validity of the address ranges, if they do not overlap each other and if the RANGE_ADDR_WIDTH match the BASE_ADDR
+# 
+#       2) inter configuration checks:
+#           a) for each bus check if it has a child bus, and if yes, 
+#              verify that the total address range of the child is contained in the right address range of the parent
+#
+#    IMPORTANT NOTE: the address range of a child bus in its configuration .csv file must be an absolute address range,
+#                    this means that if the child bus is mapped in the parent bus at the address 0x1000 to 0x1FFF, then
+#                    the peripherals in the child bus must be in the address range 0x1000 to 0x1FFF
 
 
 ####################
@@ -103,7 +118,7 @@ def check_intra_config(config : configuration.Configuration, config_file_name: s
 #############################
 # Check inter configuration #
 #############################
-# Check configuration validity between parent and son buses
+# Check configuration validity between parent and child buses
 def check_inter_config(configs : list) -> bool: 
     # For each Configuration
     for config in configs:
@@ -111,46 +126,24 @@ def check_inter_config(configs : list) -> bool:
         for i in range(config.NUM_MI):
             # If a master is a bus (is in the BUS_NAMES dict)
             if config.RANGE_NAMES[i] in BUS_NAMES.values():
-                # Find the son bus configuration
-                for son_config in configs: 
-                    if son_config.BUS_NAME == config.RANGE_NAMES[i]:
+                # Find the child bus configuration
+                for child_config in configs: 
+                    if child_config.BUS_NAME == config.RANGE_NAMES[i]:
                         # Compute the base and the end address of the parent bus
                         parent_base_address = int(config.BASE_ADDR[i], 16)
                         parent_end_address = parent_base_address + ~(~1 << (config.RANGE_ADDR_WIDTH[i]-1)) 
 
-                        # Compute the base and the end address of the son bus
-                        son_base_address = int(son_config.BASE_ADDR[0], 16)
-                        son_base_address_tmp = int(son_config.BASE_ADDR[-1], 16)
-                        son_end_address = son_base_address_tmp + ~(~1 << (son_config.RANGE_ADDR_WIDTH[i]-1)) 
+                        # Compute the base and the end address of the child bus
+                        child_base_address = int(child_config.BASE_ADDR[0], 16)
+                        child_base_address_tmp = int(child_config.BASE_ADDR[-1], 16)
+                        child_end_address = child_base_address_tmp + ~(~1 << (child_config.RANGE_ADDR_WIDTH[i]-1)) 
 
                         # Do the checks 
-                        # Check if the address space of the son is containted in the address space of the parent
-                        if son_base_address < parent_base_address or son_end_address > parent_end_address: 
-                            __print_error(f"Address of {son_config.BUS_NAME} is not properly contained in {config.BUS_NAME}")
+                        # Check if the address space of the child is containted in the address space of the parent
+                        if child_base_address < parent_base_address or child_end_address > parent_end_address: 
+                            __print_error(f"Address of {child_config.BUS_NAME} is not properly contained in {config.BUS_NAME}")
                             return False
     return True
-
-
-########################
-# Check configurations #
-########################
-def check_configs (configs : list, config_file_names : list) -> bool: 
-    status = True
-    __print(f"Starting checking {len(configs)} config...")
-    __print("Checking intra config validity")
-    for i in range(len(configs)):
-        # Intra-config check
-        status = check_intra_config(configs[i], config_file_names[i])
-
-    if status == False:
-        return False
-
-    __print("Checking intra config validity done!")
-    __print("Checking inter config validity")
-    # Inter-config check 
-    status = check_inter_config(configs)
-    __print("Checking configuration done!")
-    return status
 
 ###############
 # Read config #
@@ -196,7 +189,31 @@ def parse_args(argv : list) -> list:
 if __name__ == "__main__":
     config_file_names = parse_args(sys.argv)
     configs = read_config(config_file_names)
-    if check_configs(configs, config_file_names) == False: 
+
+    status = True
+
+    # Intra-config check
+    __print(f"Starting checking {len(configs)} config...")
+    __print("Checking intra config validity")
+    for i in range(len(configs)):
+        status = check_intra_config(configs[i], config_file_names[i])
+    
+    # Some check failed
+    if status == False:
         exit(1)
-    else:
-        exit(0)
+
+    # Success intra-config check
+    __print("Checking intra config validity done!")
+    
+    
+    # Inter-config check 
+    __print("Checking inter config validity")    
+   
+    status = check_inter_config(configs)
+    # Some check failed
+    if status == False:
+        exit(1)
+
+    # Success inter-config check
+    __print("Checking configuration done!")
+    exit(0)
