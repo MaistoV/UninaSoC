cbe786de46a09201e77ce1804dd8df25513100e9
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2025-03-12 14:57:49 +0100

    Fix for units

--- hw/units/custom_cv32e40p/fetch_sources.sh
+++ hw/units/custom_cv32e40p/fetch_sources.sh
@@ -25,6 +25,11 @@ curl --proto '=https' --tlsv1.2 https://pulp-platform.github.io/bender/init -sSf
 
 # Download dependencies (specify Target RTL and FPGA)
 printf "${YELLOW}[FETCH_SOURCES] Resolve dependencies with Bender${NC}\n"
+# Inject pre-solved conflicts
+# - package `riscv` requires `^0.1.1`
+# - package `common_cells` requires `^0.2.11`
+cp ../assets/cv32e40_Bender.lock Bender.lock
+# Checkout
 ./bender checkout
 BENDER_TARGETS="-t xilinx -t fpga"
 ./bender script flist ${BENDER_TARGETS} > rtl.flist
