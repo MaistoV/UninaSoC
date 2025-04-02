621c7c11268c677a215283e4823ca7539da1497e
Gr4npasso <stefanomercogliano@gmail.com>
2025-04-02 09:59:20 +0200

    [PR] Reverting create_linker_script

--- config/scripts/create_linker_script.py
+++ config/scripts/create_linker_script.py
@@ -1,7 +1,7 @@
 #!/bin/python3.10
 # Author: Stefano Toscano 		<stefa.toscano@studenti.unina.it>
 # Author: Vincenzo Maisto 		<vincenzo.maisto2@unina.it>
-# Author: Stefano Mercogliano 	<stefano.mercogliano@unina.it>
+# Author: Stefano Mercogliano 		<stefano.mercogliano@unina.it>
 # Description:
 #   Generate a linker script file from the CSV configuration.
 # Note:
