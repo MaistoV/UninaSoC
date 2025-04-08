7923e2e3c1819dba265c8a1f53f44e56c101920c
Stefano Mercogliano <stefanomercogliano@gmail.com>
2025-04-08 15:41:52 +0200

    [quickfix/ibex_fetch_sources] Removing python dependencies from Ibex (#100)
    In this PR we removed the fusesoc usage from `custom_ibex`
    `fetch_sources.sh` script as it was not compatible with the project
    reccomended version of Python (3.10). Instead, we added an `assets` repo
    with fusesoc pre-generated filelist.
    
    ---------
    
    Co-authored-by: Vincenzo Maisto <vincenzo.maisto2@unina.it>

--- hw/units/custom_ibex/fetch_sources.sh
+++ hw/units/custom_ibex/fetch_sources.sh
@@ -10,38 +10,45 @@ NC='\033[0m' # No Color
 
 
 # Create rtl dir
-mkdir rtl
+mkdir -p rtl
 
 # clone repo (Ibex releases are inconsistent, therefore we must stick to a specific commit).
-GIT_URL=https://github.com/lowRISC/ibex.git
-GIT_TAG=master
-GIT_COMMIT=6e466c1
+GIT_URL=git@github.com:lowRISC/ibex.git
+GIT_BRANCH=master
+GIT_COMMIT=6e466c1504b61333d1b2ba85c69bff94b65e9284
 CLONE_DIR=ibex
 printf "${YELLOW}[FETCH_SOURCES] Cloning source repository${NC}\n"
-git clone ${GIT_URL} -b ${GIT_TAG} --depth 1 ${CLONE_DIR}
+git clone ${GIT_URL} -b ${GIT_BRANCH} ${CLONE_DIR}
 cd ${CLONE_DIR};
 git reset --hard ${GIT_COMMIT}
 
-# setuptools, pip and Python 3.6 is required.
-echo -e "${YELLOW}[FETCH_SOURCES] Checking and installing Python modules ... ${NC}"
-pip3 install --upgrade --user fusesoc
-pip3 install -U -r python-requirements.txt
-fusesoc --cores-root . run --target=lint --setup --build-root ./build/ibex_out lowrisc:ibex:ibex_top
-
-echo -e "${YELLOW}[FETCH_SOURCES] Clone sources to RTL ${NC}"
-find "$PWD/build/ibex_out/src" -type f -name "*.sv" >> ../flist
-find "$PWD/build/ibex_out/src" -type f -name "*.svh" >> ../flist
-cd ..;
-
 # Copy all RTL files into rtl dir
+# We use the flist pre-generated with fusesoc and saved in assets
+# the flist is generated using:
+#   fusesoc --cores-root . run --target=lint --setup --build-root ./build/ibex_out lowrisc:ibex:ibex_top
+#   Python version 3.6
+cd ..;
+FLIST="$PWD/assets/flist"
+LOOKUP_DIR="$PWD/ibex"
+RTL_DIR="$PWD/rtl"
+
 printf "${YELLOW}[FETCH_SOURCES] Copy all sources into rtl${NC}\n" s
-for rtl_file in $(cat flist) ; do
-    cp $rtl_file rtl
-done;
+while IFS= read -r filename; do
+    # Find the file in LOOKUP_DIR
+    filepath=$(find "$LOOKUP_DIR" -type f -name "$filename" 2>/dev/null | head -n 1)
+
+    # If found
+    if [ -n "$filepath" ]; then
+        cp "$filepath" "$RTL_DIR/"
+    # Error
+    else
+        printf "${RED}[FETCH_SOURCES] Error: $filename not found in $LOOKUP_DIR${NC}\n"
+        exit 1
+    fi
+done < "$FLIST"
 
 # Delete the cloned repo and temporary flist
 printf "${YELLOW}[FETCH_SOURCES] Clean all artifacts${NC}\n"
 sudo rm -r ${CLONE_DIR}
-rm flist
 printf "${GREEN}[FETCH_SOURCES] Completed${NC}\n"
 
