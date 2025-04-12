4a5644f524c28648c611a4ea14aef5faa756bc91
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2025-04-12 13:29:48 +0200

    [custom] Switch to v1_1

--- settings_custom.sh
+++ settings_custom.sh
@@ -1,4 +1,5 @@
 source settings.sh embedded nexys_a7_50t
 
-cp -v ${CONFIG_ROOT}/configs/${SOC_CONFIG}/config_main_bus_hls_gemm_v1_0.csv \
+# cp -v ${CONFIG_ROOT}/configs/${SOC_CONFIG}/config_main_bus_hls_gemm_v1_0.csv \
+cp -v ${CONFIG_ROOT}/configs/${SOC_CONFIG}/config_main_bus_hls_gemm_v1_1.csv \
 		${CONFIG_ROOT}/configs/${SOC_CONFIG}/config_main_bus.csv
