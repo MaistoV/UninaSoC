cbe5a842854af832f0b12daca4c3189a94b5a841
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2025-04-16 20:04:14 +0200

    [tmp] Push utility scripts

---
+++ readback.sh
@@ -0,0 +1,1 @@
+make -C hw/xilinx/ readback_jtag2axi OFFSET=0x3ec0 NUM_BYTES=$((65*4))
--- settings_custom.sh
+++ settings_custom.sh
@@ -1,5 +1,6 @@
-source settings.sh embedded nexys_a7_50t
+source settings.sh embedded nexys_a7_100t
 
 # cp -v ${CONFIG_ROOT}/configs/${SOC_CONFIG}/config_main_bus_hls_gemm_v1_0.csv \
-cp -v ${CONFIG_ROOT}/configs/${SOC_CONFIG}/config_main_bus_hls_gemm_v1_1.csv \
+# cp -v ${CONFIG_ROOT}/configs/${SOC_CONFIG}/config_main_bus_hls_gemm_v1_1.csv \
+cp -v ${CONFIG_ROOT}/configs/${SOC_CONFIG}/config_main_bus_hls_vdotprod.csv \
 		${CONFIG_ROOT}/configs/${SOC_CONFIG}/config_main_bus.csv
