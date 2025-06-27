2a5be263e910f40fcd45c47f019c4b06b7b5de25
Gr4npasso <stefanomercogliano@gmail.com>
2025-06-27 13:38:06 +0200

    [Update] Check

--- hw/xilinx/synth/tcl/mark_debug_nets.tcl
+++ hw/xilinx/synth/tcl/mark_debug_nets.tcl
@@ -4,4 +4,4 @@
 # System master AXI interface
 #set_property MARK_DEBUG 1 [get_nets sys_master_u/m_axi_*]
 set_property MARK_DEBUG 1 [get_nets rv_socket_u/rv_socket_data_*]
-set_property MARK_DEBUG 1 [ddr4_channel_0_wrapper_u/s_axi*]
+set_property MARK_DEBUG 1 [get_nets ddr4_channel_0_wrapper_u/s_axi*]
